b04c847806a17b6c473ea590083f8d0b1aac4f9e
Azeez Samad <seshp@ymail.com>
2022-04-13 19:57:21 +0100

    add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+
+
+  return (
+    <div>
+      <div className="navbar">
+          <h1>Supreme blog</h1>
+          <a href="/">Home</a>
+          <a href="/create">Create new</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
