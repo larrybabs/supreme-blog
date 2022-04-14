6072ea4896ab6f051f139d6bc48d9a735a2c979a
Azeez Samad <seshp@ymail.com>
2022-04-14 14:18:20 +0100

    add routes for homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-  
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
